d889b2b8c78b31d8712f73c60772131a326a3918
JosephWaldron <waldrojo@kean.edu>
2022-01-28 02:06:17 -0500

    done

---
+++ variables.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string characterName = "John";
+    int characterAge;
+    characterAge = 25;
+    cout << "Name: " << characterName << endl;
+    cout << "Age: " << characterAge << endl;
+
+    char grade = 'A';
+    string phrase = "string";
+    int age = 50;
+    float decimal = 2.3;
+    double bigger = 2.333;
+    bool boolean = false;
+
+
+    return 0;
+}
