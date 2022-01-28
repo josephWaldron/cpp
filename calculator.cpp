6ab2824144862fa273b12a3207e4a00c5dac7377
JosephWaldron <waldrojo@kean.edu>
2022-01-28 17:38:06 -0500

    calc

--- calculator.cpp
+++ calculator.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
+    int num1, num2;
+    cout << "Enter number 1: ";
+    cin >> num1;
+    cout << "Enter number 2: ";
+    cin >> num2;
+    cout << num1 + num2;
     
+
     return 0;
 }
